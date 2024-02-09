b20bc46d8869dcd9b981a6071406d8775ff8e830
Agu Peace <agupeace21.04@gmail.com>
2024-02-09 01:20:31 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>
-    <h1><b>I am Peace Agu</b></h1>
-    <p>I am a <strong>programmer who loves coding</strong>
-    <p><small><mark>Programming</mark> is a skill that requires grit </small></p>
-      <p>I don't plan to <del>Quit</del> anytime soon.</p>
-      <p>I love drinking H<sub>2</sub>0 and I <i>love to</i> breathe in O<sup>2</sup></p>
-      <p> I also love <b>JESUS</b> Yess </p>
-    </p>
-  </>;
-  
+	return (
+		<>
+			<h1>I am Peace Agu</h1>
+			<p>
+				I love to <b>code as much as possible.</b>
+			</p>
+			<p>
+				I love drinking<i> a lot of</i> H<sub>2</sub>0 and I <i>breathe</i> in O<sup>2</sup>
+			</p>
+		</>
+	);
 }
