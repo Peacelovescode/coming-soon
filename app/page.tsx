1e2cd931445a2daebb2a18d6cc9ab859751bc597
Agu Peace <agupeace21.04@gmail.com>
2024-02-22 11:13:28 +0100

    Update to Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,15 @@ export default function Home() {
 			<p>
 				I love drinking<i> a lot of</i> H<sub>2</sub>0 and I <i>breathe</i> in O<sup>2</sup>
 			</p>
+			<p>Print this page by pressing <kbd>ctrl + p </kbd> on your keyboard</p>
+			<p>Peace Loves coding in <pre><code>html, css, javascript, react, next.js</code></pre></p>
+			<p>
+				I really really so much hate Tinubu of <abbr title="ALL PROGRESSIVE CONGRESS">APC</abbr>
+			</p>
+			<p> I live at <address>No 20, Ebenezer street, Idimu, <abbr title="UNITED KINGDOM">UK</abbr></address></p>
+			<p>
+				<bdo dir="ltr">This is a left to right and can be right to left</bdo>
+			</p>
 		</>
 	);
 }
